63c6648d04b041836658205b9a81701a5fcfa4df
stainsleto <stainsmault@gmail.com>
2023-02-28 19:31:02 +0530

    @Stains/ Added random images

--- src/components/memeForm.js
+++ src/components/memeForm.js
@@ -1,24 +1,39 @@
 import React from 'react'
+import memesData from './memesData'
+
 
 function MemeForm(){
-    const [memeImage,setMemeImage] = React.useState('')
+    // const [memeImage,setMemeImage] = React.useState('https://i.imgflip.com/30b1gx.jpg')
+
+    const [meme,setMeme] = React.useState({
+        topText: '',
+        bottomText: '',
+        randomImage : 'https://i.imgflip.com/30b1gx.jpg'
+    })
+
+    const[allMemeImages,setAllMemeImages] = React.useState(memesData)
+
     function getMemeImage(){
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage : url
+        }))
     }
     return(
         <div>
             <div className="form-div">
                 <form>
-                    <input className="input-box" type="text"  />
-                    <input className="input-box" type="text" /><br></br>
-                    <input className="submit-button"type="submit" value="Get a new meme image🖼️" />
+                    <input className="input-box" type="text" placeholder="Top text" />
+                    <input className="input-box" type="text" placeholder="Bottom text" /><br></br>
+                    <button className="submit-button" onClick={getMemeImage}> Get a new meme image🖼️ </button>
                 </form>
             </div>
             <div>
 
-                <img src="{memeImage}" />
+                <img src={meme.randomImage} className="meme--image" />
 
             </div>
         </div>
